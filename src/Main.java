b03473f533a6ce0dadd6f97d58cfcaee044e1ce9
Alexey Osipov <snow_man@bk.ru>
2019-12-22 22:43:20 +0400

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        //int c = a + b;
+
+        System.out.println("a > b  = " + (a > b));
+        System.out.println("a < b  = " + (a < b));
+
+    }
+}
